2c9edcf6d62b6ec8124fd6b07463711cb3d6eef4
User <User@DESKTOP-D05IIM6>
2019-05-14 20:07:39 +0300

    a

--- Project_Bulls_and_cows/Header.h
+++ Project_Bulls_and_cows/Header.h
@@ -1,2 +1,38 @@
 #pragma once
-int bullcaw(int arc[], int size, int popitka);
+int Core(int arc[], int size, int popitka);
+void printResult();
+void Help();
+void Exit();
+using namespace std;
+
+struct GameResult
+{
+	int cow;
+	int bull;
+	int Try;
+}GameResult1;
+
+void printResult()
+{
+	cout << endl << "Кол-о коров = " << GameResult1.cow << endl;
+	cout << endl << "Кол-о быков = " << GameResult1.bull << endl;
+	cout << "Кол-во попыток = " << GameResult1.Try;
+}
+
+void Help()
+{
+	system("cls");
+	cout << "Программа загадывает четырёхзначное число и играющий должен угадать его.\n\
+После ввода пользователем числа программа сообщает,\nсколько цифр числа угадано(быки) и сколько цифр угадано\
+и стоит на нужном месте(коровы).\nПосле отгадывания числа на экран необходимо вывести количество сделанных\
+пользователем попыток.";
+	system("pause>NULL");
+	system("cls");
+}
+
+void Exit() {
+	system("cls");
+	printResult();
+	system("pause>NULL");
+	exit(0);
+}
--- Project_Bulls_and_cows/Source.cpp
+++ Project_Bulls_and_cows/Source.cpp
@@ -3,58 +3,68 @@
 #include <time.h>
 using namespace std;
 
-/*Написать игру «Быки и коровы». Программа "загадывает" четырёхзначное число и играющий должен угадать его. 
-После ввода пользователем числа программа сообщает, сколько цифр числа угадано (быки) и сколько цифр угадано
-и стоит на нужном месте (коровы). После отгадывания числа на экран необходимо вывести количество сделанных 
-пользователем попыток. В программе необходимо использовать рекурсию.*/
-
-
 void main()
 {
-	int popitka = 1;
-	int ugadai;
-	const int size = 4;
 	setlocale(0, "RUS");
 	srand(time(NULL));
-	ugadai = rand() % 9000 + 1000;
+
+	//Начальные значения:
+	GameResult1.cow = 0;
+	GameResult1.bull = 0;
+	GameResult1.Try = 1;
+	int ugadai;
+	const int size = 4;
+	ugadai = rand() % 9999 + 1000;
 	int arc_ugadai[size];
-	//cout <<"Загаданное число -> "<< ugadai<<endl;
+	
+	Help();       //Старт..
+	
+
+	//Получение поэлементно цифр загадонного числа:
+	//Запихиваем их в массив аrc_ugadai
 	for (int i = 0; i < size; i++)
 	{
 		arc_ugadai[i] = ugadai % 10;
 		ugadai = ugadai / 10;
 	}
 
-	bullcaw(arc_ugadai, 4, popitka);
-
-	system("pause");
+	Core(arc_ugadai, 4, GameResult1.Try);
+	Exit();
+	system("pause>NULL");
 }
 
-int bullcaw(int arc[], int size, int popitka)
+int Core(int arc[], int size, int popitka)
 {
-	int cow = 0, bull = 0;
-	cout << endl << endl;
+	GameResult1.Try = popitka;
 	int chislo;
 	int arc_chislo[4];
+	//int cow = 0, bull = 0;
+	cout << endl << endl;
 	cout << "Please insert number minimum 1000 ->";
 	cin >> chislo;
+
+	if (chislo == 0) Exit();
 
 	for (int i = 0; i < size; i++)
 	{
 		arc_chislo[i] = chislo % 10;
 		chislo = chislo / 10;
-		if (arc_chislo[i] == arc[i]) cow++;
+		if (arc_chislo[i] == arc[i]) GameResult1.cow++;
 		for (int j = 0; j < size; j++)
 		{
-			if (arc_chislo[i] == arc[j]) bull++;
-			if (bull > 4) break;
+			if (arc_chislo[i] == arc[j]) GameResult1.bull++;
+			if (GameResult1.bull > 4) break;
 		}
 		cout << arc[i] << " " << arc_chislo[i] << endl;
 	}
 
-	cout << endl << "Кол-о коров = " << cow << endl;
-	cout << endl << "Кол-о быков = " << bull << endl;
-	cout << "Кол-во попыток = " << popitka;
-	popitka++;
-	if (cow != 4 && bull != 4) return bullcaw(arc, 4, popitka);
+	printResult();
+	GameResult1.Try++;
+
+	//Если быки и коровы !=4 -> Запускаем рекурсию: 
+	if (GameResult1.cow != 4 && GameResult1.bull != 4) return Core(arc, 4, GameResult1.Try);
 }
+
+
+
+
